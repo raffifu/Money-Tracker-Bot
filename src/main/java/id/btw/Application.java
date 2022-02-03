19d36cfff779ce1307efb3cf2db98a787d4cd9dc
Raffi Utama <raffifebriandikau@gmail.com>
2022-02-04 00:24:15 +0700

    Add logback

--- src/main/java/id/btw/Application.java
+++ src/main/java/id/btw/Application.java
@@ -1,13 +1,17 @@
 package id.btw;
 
 import id.btw.bot.MoneyTrackerBot;
+import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@Slf4j
 public class Application {
     public static void main(String[] args) {
         try {
+            log.info("Bot started");
+
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new MoneyTrackerBot());
         } catch (TelegramApiException e) {
--- src/main/java/id/btw/bot/MoneyTrackerBot.java
+++ src/main/java/id/btw/bot/MoneyTrackerBot.java
@@ -1,9 +1,12 @@
 package id.btw.bot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Slf4j
 public class MoneyTrackerBot extends TelegramLongPollingBot {
+
     @Override
     public String getBotUsername() {
         return System.getenv("BOT_USERNAME");
@@ -16,6 +19,7 @@ public class MoneyTrackerBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        
+        if (update.hasMessage() && update.getMessage().hasText())
+            log.info("Received Message {}", update.getMessage().getText());
     }
 }
