14cfee1a8c273d84592145ad8455c21c3acdded2
Raffi Utama <raffifebriandikau@gmail.com>
2022-04-04 08:35:16 +0700

    Implement logic edit date
    - Add callback handler for edit date
    - Add date pad

--- src/main/java/id/my/btw/service/CallbackService.java
+++ src/main/java/id/my/btw/service/CallbackService.java
@@ -14,6 +14,8 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.Optional;
 import java.util.function.Consumer;
 
@@ -21,8 +23,9 @@ import java.util.function.Consumer;
 public class CallbackService {
     public static final String DELETE = "DELETE";
     public static final String CONFIRM_DELETE = "CONFIRM_DELETE";
-    public static final String CANCEL_DELETE = "CANCEL_DELETE";
+    public static final String CANCEL = "CANCEL";
     public static final String EDIT_CATEGORY = "EDIT_CATEGORY";
+    public static final String EDIT_DATE = "EDIT_DATE";
 
     private final ExpenseRepository expenseRepository;
 
@@ -33,31 +36,40 @@ public class CallbackService {
     public void handleIncomingCallback(CallbackQuery callbackQuery, Consumer<? super BotApiMethod<? extends Serializable>> responseHandler) throws TelegramApiException {
         Message message = callbackQuery.getMessage();
 
+        log.info("[{}] - Handling Callback", callbackQuery.getData());
+
         switch (callbackQuery.getData()) {
             case DELETE:
                 responseHandler.accept(onDelete(message));
                 break;
             case CONFIRM_DELETE:
-                responseHandler.accept(onConfirmDelete(message));
+                responseHandler.accept(onConfirmedDelete(message));
                 break;
-            case CANCEL_DELETE:
-                responseHandler.accept(onCancelDelete(message));
+            case CANCEL:
+                responseHandler.accept(onCancel(message));
                 break;
             case EDIT_CATEGORY:
                 responseHandler.accept(onEditCategory(message));
                 break;
+            case EDIT_DATE:
+                responseHandler.accept(onEditDate(message));
+                break;
             default:
-                responseHandler.accept(onInputCategory(message, callbackQuery.getData()));
+                responseHandler.accept(onGeneralCallback(message, callbackQuery.getData()));
         }
     }
 
     private EditMessageText onDelete(Message message) {
+        log.info("Show delete confirmation");
+
         EditMessageText editMessage = ResponseUtil.genEditMessage(message);
         editMessage.setReplyMarkup(KeyboardUtil.confirmDeletePad());
         return editMessage;
     }
 
-    private EditMessageText onConfirmDelete(Message message) throws TelegramApiException {
+    private EditMessageText onConfirmedDelete(Message message) throws TelegramApiException {
+        log.info("Deleting expense: {}", message.getReplyToMessage().getMessageId());
+
         Integer id = Optional
                 .ofNullable(CommonUtil.getId(message.getText()))
                 .orElseThrow(() -> new TelegramApiException("Invalid Message"));
@@ -67,19 +79,82 @@ public class CallbackService {
         return ResponseUtil.genEditMessage(message, "Deleted from database");
     }
 
-    private EditMessageText onCancelDelete(Message message) {
+    private EditMessageText onCancel(Message message) {
+        log.info("Show default pad");
+
         EditMessageText editMessage = ResponseUtil.genEditMessage(message);
         editMessage.setReplyMarkup(KeyboardUtil.defaultPad());
         return editMessage;
     }
 
     private EditMessageText onEditCategory(Message message) {
+        log.info("Show category pad");
+
         EditMessageText editMessage = ResponseUtil.genEditMessage(message);
         editMessage.setReplyMarkup(KeyboardUtil.categoryPad());
         return editMessage;
     }
 
+    private EditMessageText onEditDate(Message message) throws TelegramApiException {
+        log.info("Show date pad");
+
+        Integer id = Optional
+                .ofNullable(CommonUtil.getId(message.getText()))
+                .orElseThrow(() -> new TelegramApiException("Invalid Message"));
+
+        Expense expense = Optional
+                .ofNullable(expenseRepository.getById(id))
+                .orElseThrow(() -> new TelegramApiException("Invalid Message"));
+
+        EditMessageText editMessage = ResponseUtil.genEditMessage(message);
+        editMessage.setReplyMarkup(KeyboardUtil.datePad(expense.getDate()));
+        return editMessage;
+    }
+
+    private EditMessageText onGeneralCallback(Message message, String callbackData) throws TelegramApiException {
+        log.info("Callback Data not found, handling as general callback");
+
+        EditMessageText editMessage = null;
+
+        if (isInCategory(callbackData)) {
+            editMessage = onInputCategory(message, callbackData);
+        } else if (isDate(callbackData)) {
+            editMessage = onInputDate(message, callbackData);
+        }
+
+        return Optional
+                .ofNullable(editMessage)
+                .orElseThrow(() -> new TelegramApiException("Invalid Callback"));
+    }
+
+    private EditMessageText onInputDate(Message message, String callbackData) throws TelegramApiException {
+        log.info("Input date: {}", callbackData);
+
+        Integer id = Optional
+                .ofNullable(CommonUtil.getId(message.getText()))
+                .orElseThrow(() -> new TelegramApiException("Invalid Message"));
+
+        Expense expense = Optional
+                .ofNullable(expenseRepository.getById(id))
+                .orElseThrow(() -> new TelegramApiException("Invalid Message"));
+
+        LocalDate newDate = LocalDate.parse(callbackData);
+        if (newDate.isAfter(LocalDate.now())) {
+            log.error("Date is in the future");
+            throw new TelegramApiException("Invalid Date");
+        }
+
+        expense.setDate(newDate);
+        expenseRepository.update(expense);
+
+        EditMessageText editMessage = ResponseUtil.genEditMessage(message, ResponseUtil.genResultMessage(expense));
+        editMessage.setReplyMarkup(KeyboardUtil.defaultPad());
+        return editMessage;
+    }
+
     private EditMessageText onInputCategory(Message message, String callbackData) throws TelegramApiException {
+        log.info("Input category: {}", callbackData);
+
         Category category = Category.valueOf(callbackData);
         Integer id = Optional
                 .ofNullable(CommonUtil.getId(message.getText()))
@@ -95,5 +170,23 @@ public class CallbackService {
         EditMessageText editMessage = ResponseUtil.genEditMessage(message, ResponseUtil.genResultMessage(expense));
         editMessage.setReplyMarkup(KeyboardUtil.defaultPad());
         return editMessage;
+    }
+
+    private Boolean isDate(String date) {
+        try {
+            LocalDate.parse(date);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    private Boolean isInCategory(String data) {
+        for (Category category : Category.values()) {
+            if (category.name().equals(data)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
